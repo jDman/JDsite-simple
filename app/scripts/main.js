6b14832911a0c9480400d01330ff6a4ff9c1f8fb
Jessie Shi <jessie.shi@me.com>
2015-05-04 22:09:36 +0100

    Updates with Meteor app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,39 +1,20 @@
 'use strict';
 
-var inviewHandler = function($ele) {
-  if ($(window).scrollTop() + $(window).height() >= $ele.offset().top) {
-      if(!$ele.attr('inview')) {
-          $ele.attr('inview', true);
-          $ele.hide().fadeIn(3000);
-      }
-  }
-};
-
-
 (function () {
   var navMenu = $('nav ul'),
-      isDisplayed = 'is-displayed-mobile',
       isCollapsed = 'is-collapsed-mobile';
   var menuToggle = function () {
     if(navMenu.hasClass(isCollapsed)){
       navMenu.removeClass(isCollapsed);
-      navMenu.addClass(isDisplayed);
     } else {
-      navMenu.removeClass(isDisplayed);
       navMenu.addClass(isCollapsed);
     }
   };
 
-  var $proj = $('#projects');
 
-  $('#projects').hide();
   $('#toggle').click(menuToggle);
   $('.nav__item').click(function () {
     menuToggle();
   });
-  $(window).scroll(function(){
-    inviewHandler($proj);
-  });
-
 
 })();
